31843a8227dbaa3b2cfd50a453ae8f00db149331
i7lam <islatagirov@gmail.com>
2021-07-18 02:39:41 +0300

    Третий шаг

---
+++ brain_games/cli.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.9
+import prompt
+def welcome_user():
+    name = prompt.string('May I have your name? ')
+    print('Hello,', name, end='')
+    print('!')
+
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3.9
+from brain_games.cli import welcome_user
 def fp(arg):
     print(arg)
 def main():
     fp('Welcome to the Brain games!')
+    welcome_user()
 if __name__ == '__main__':
     main()
