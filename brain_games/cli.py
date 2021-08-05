def2db82b46d9f26178cb461531473343d5f56b4
i7lam <islatagirov@gmail.com>
2021-08-06 00:24:47 +0300

    Для .yaml

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3.9
 import prompt
+
+
 def welcome_user():
     name = prompt.string('May I have your name? ')
     print('Hello,', name, end='')
     print('!')
-
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3.9
 from brain_games.cli import welcome_user
-def fp(arg):
-    print(arg)
+
+
 def main():
-    fp('Welcome to the Brain games!')
+    print('Welcome to the Brain games!')
     welcome_user()
+
+
 if __name__ == '__main__':
     main()
